8692846dca3b6875085c53cf8b91ca64bef89a5a
Hsing Lo <hsing@flyingbat.net>
2024-04-01 00:53:13 -0400

    even more functional again

--- render-subseq.js
+++ render-subseq.js
@@ -3,7 +3,7 @@ const boardTemplate = '<div class="board"><div class="boardrow"><div class="boar
 
 const blockAnchor = [0, 3, 6, 27, 30, 33, 54, 57, 60]
 const blockOffset = [0, 1, 2, 9, 10, 11, 18, 19, 20]
-const fillOrder = Array(81).fill().map((v, i) => blockAnchor[Math.floor(i / 9)] + blockOffset[i % 9])
+const fillOrder = i => blockAnchor[Math.floor(i / 9)] + blockOffset[i % 9]
 
 render()
 // let data = ""
@@ -16,17 +16,17 @@ function render() {
     const dataArray = data.split('~').slice(0, -3)
     const puzzleCount = dataArray.length / 4
 
-    const boards = Array(puzzleCount).fill().map((v, i) => dataArray[i * 4 + 0])
-    const solutions = Array(puzzleCount).fill().map((v, i) => dataArray[i * 4 + 1])
-    const removes = Array(puzzleCount).fill().map((v, i) => dataArray[i * 4 + 2])
-    const checkercalls = Array(puzzleCount).fill().map((v, i) => dataArray[i * 4 + 3])
+    const boards = i => dataArray[i * 4 + 0]
+    const solutions = i => dataArray[i * 4 + 1]
+    const removes = i => dataArray[i * 4 + 2]
+    const checkercalls = i => dataArray[i * 4 + 3]
 
-    const boardFeedPuzzle = (v, i) => `<p>${i + 1}. ${removes[i]} removed. ${checkercalls[i]} checker calls.</p>` 
-        + Array(81).fill().flatMap((v, j) => [boardTemplate[j], boards[i][fillOrder[j]].replace('0', ' ')]).join("") 
+    const boardFeedPuzzle = (v, i) => `<p>${i + 1}. ${removes(i)} removed. ${checkercalls(i)} checker calls.</p>` 
+        + Array(81).fill().flatMap((v, j) => [boardTemplate[j], boards(i)[fillOrder(j)].replace('0', ' ')]).join("") 
         + boardTemplate[81]
 
     const solutionFeedPuzzle = (v, i) => `<p>${i + 1}. solution</p>` 
-        + Array(81).fill().flatMap((v, j) => [boardTemplate[j], solutions[i][fillOrder[j]]]).join("") 
+        + Array(81).fill().flatMap((v, j) => [boardTemplate[j], solutions(i)[fillOrder(j)]]).join("") 
         + boardTemplate[81]
 
     document.getElementById('boards').innerHTML = Array(puzzleCount).fill().map(boardFeedPuzzle).join('')
